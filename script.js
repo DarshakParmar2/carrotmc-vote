8511b1a6bad54e6381aede06faa178b17d9e58d1
DarshakParmar79 <darshakparmar795@gmail.com>
2024-04-06 12:22:25 +0530

    Refactor vote buttons and script.js***

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-function openVoteLink(url) {
-    window.open(url, '_blank');
-}
 // Hide loader when page finishes loading
 window.onload = function() {
     document.querySelector('.loader').style.display = 'none';
 };
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    var buttons = document.querySelectorAll(".vote-buttons button");
+    buttons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            openVoteLink(this.getAttribute("data-url"));
+        });
+    });
+});
+
+function openVoteLink(url) {
+    window.open(url, '_blank');
+}
+
+
+// Hide loader when page finishes loading
+window.onload = function() {
+    document.querySelector('.loader').style.display = 'none';
+};
+
